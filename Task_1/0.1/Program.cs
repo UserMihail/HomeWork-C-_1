f0cae11601703f5d925924045dee207a2938dc92
Mihail <horunzhy.m@yandex.ru>
2022-11-25 13:29:58 +0300

    task #2

---
+++ Task_1/0.1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу которая принимет на вход
+// три числа и выдает максимаьлное из этих чисел.
+
+Console.WriteLine("Введите число");
+
+int a = 44;
+int b = 5;
+int c = 78;
+
+int max = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (c > max ) max = c;
+
+Console.Write("max= ");
+Console.WriteLine(max);
